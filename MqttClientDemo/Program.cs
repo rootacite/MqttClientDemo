ede366ab425dac79267b7b69c316225f05dd30b6
世元 李 <1498045907@qq.com>
2023-03-27 01:59:10 +0800

    2023.3.27

--- MqttClientDemo/Program.cs
+++ MqttClientDemo/Program.cs
@@ -17,19 +17,28 @@ internal class Program
         var mqttClient = new MqttFactory().CreateMqttClient();
 
         var client_options = new MqttClientOptionsBuilder()
+         .WithClientId("Demo_0")
          .WithTcpServer("127.0.0.1", 34420) // Port is optional
          .WithCredentials("root", "07211145141919")
         .Build();
 
-        var r_cont =  mqttClient.ConnectAsync(client_options, CancellationToken.None);
+        var r_cont = mqttClient.ConnectAsync(client_options, CancellationToken.None);
 
 
         while (!r_cont.IsCompleted) { }
 
-        if(r_cont.IsFaulted)
+        if (r_cont.IsFaulted)
         {
             WriteLine(r_cont.Exception.Message);
+            return;
         }
+
+        mqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+    .WithTopic("Sys/ServiceMsg")
+    .WithPayload("Hello World")
+    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.AtLeastOnce)
+    .WithRetainFlag(true)
+    .Build(), CancellationToken.None);
         ReadKey();
     }
 }
